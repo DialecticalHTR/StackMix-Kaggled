e7ff5d8924095d0d8b95a47aa42924d8408fce13
Alexander Starikov <acherryjam@gmail.com>
2025-03-05 03:30:05 +0300

    add symbols to dialectic dataset

--- configs/dialectic.py
+++ configs/dialectic.py
@@ -9,7 +9,7 @@ class DialecticConfig(BaseConfig):
             image_w=2048,
             image_h=128,
             dataset_name='dialectic',
-            chars=' !"%\'()+,-./0123456789:;=?R[]abcehinoprstuxy«»АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюяё№\u0301\u0302\u0304\u0306\u0311‿',
+            chars=' !"%\'()+,-./0123456789:;=?R[]abcehinoprstuxy«»АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюяё№\u0301\u0302\u0304\u0306\u0311‿jЪЫЬЁ#Il',
             corpus_name='jigsaw_corpus.txt',
             blank='ß',
             **kwargs,
